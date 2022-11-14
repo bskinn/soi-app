49a35028b0b91b6dc0d7362d3f4d770a24734da2
Brian Skinn <brian.skinn@gmail.com>
2022-11-14 18:24:57 -0500

    (WIP) Start trying to do continuous update

--- app.py
+++ app.py
@@ -19,9 +19,16 @@ BTN_SEARCH = "button-search"
 
 RESULT_DISPLAY = "result-display"
 
+INTERVAL_OUTPUT_TIMER = "interval-output-timer"
+INTERVAL_STATS = "interval-stats"
 
-def divver(iterable):
-    return list(iterable)
+
+proc = None
+buf_stdout = ""
+
+
+def code_ify(text):
+    return f"```none\n{text}\n```"
 
 
 app.layout = dhtml.Div(
@@ -29,18 +36,20 @@ app.layout = dhtml.Div(
         dhtml.Div(
             [
                 dhtml.Span("URL"),
-                dcc.Input(type="url", size="80", id=INPUT_URL),
+                dcc.Input(type="url", size="80", id=INPUT_URL, required=True),
             ]
         ),
         dhtml.Div(
             [
                 dhtml.Span("Search Term"),
-                dcc.Input(type="text", size="45", id=INPUT_SEARCH),
+                dcc.Input(type="text", size="45", id=INPUT_SEARCH, required=True),
             ]
         ),
         dhtml.Button("Search", id=BTN_SEARCH),
+        dcc.Interval(id=INTERVAL_OUTPUT_TIMER, interval=300, disabled=True),
+        dhtml.Div("", id="interval-stats"),
         dcc.Markdown(
-            "```\nLoading...\n```",
+            code_ify("Awaiting input..."),
             id=RESULT_DISPLAY,
             style={
                 "font-family": "monospace",
@@ -54,26 +63,63 @@ app.layout = dhtml.Div(
 
 @app.callback(
     dash.Output(RESULT_DISPLAY, "children"),
+    dash.Output(INTERVAL_OUTPUT_TIMER, "disabled"),
+    dash.Output(INTERVAL_STATS, "children"),
     dash.Input(BTN_SEARCH, "n_clicks"),
     dash.Input(INPUT_URL, "n_submit"),
     dash.Input(INPUT_SEARCH, "n_submit"),
+    dash.Input(INTERVAL_OUTPUT_TIMER, "n_intervals"),
     dash.State(INPUT_URL, "value"),
     dash.State(INPUT_SEARCH, "value"),
+    dash.State(RESULT_DISPLAY, "children"),
 )
-def run_suggest(n_clicks, n_submit_url, n_submit_search, url_value, search_value):
-    result = sp.run(
-        ["sphobjinv", "suggest", "-uas", str(url_value), str(search_value)],
-        capture_output=True,
-        text=True,
-    )
+def run_suggest(
+    n_clicks,
+    n_submit_url,
+    n_submit_search,
+    n_timer_intervals,
+    url_value,
+    search_value,
+    result_text,
+):
+    global proc, buf_stdout
 
-    if result.returncode > 1:
-        return "It errored"
+    if proc is None:
+        # suggest operation is not running, so we should start it
+        if url_value is None or search_value is None:
+            # Don't start the search process if either input is missing; disabled=True
+            return (result_text, True, f"Off ({n_timer_intervals})")
 
-    if url_value and search_value:
-        return f"```none\n{result.stderr}\n{result.stdout}\n```"
+        buf_stdout += "\nStarting proc"
+        proc = sp.Popen(
+            ["sphobjinv", "suggest", "-uas", str(url_value), str(search_value)],
+            stdout=sp.PIPE,
+            stderr=sp.STDOUT,
+            text=True,
+        )
+
+        # Turn on the Interval; set disabled to False
+        return (code_ify(buf_stdout), False, f"On ({n_timer_intervals})")
+
     else:
-        return f"```none\nEnter search values\n```"
+        # suggest operation IS running, which means we should update it
+        rc = proc.poll()
+        # buf_stdout += data
+        
+        if rc:
+            proc = None
+            if rc != 0:
+                return (code_ify(f"{buf_stdout}\n\nIt errored ({rc=})"), True, f"Off ({n_timer_intervals})")
+            else:
+                return (code_ify(buf_stdout), True, f"Off ({n_timer_intervals})")
+        else:
+            buf_stdout = proc.stdout.read()
+            return (code_ify(buf_stdout), False, f"On ({n_timer_intervals})")
+
+        # if url_value and search_value:
+        #     return f"```none\n{result.stderr}\n{result.stdout}\n```"
+        # else:
+        #     return f"```none\nEnter search values\n```"
 
 
 if __name__ == "__main__":
